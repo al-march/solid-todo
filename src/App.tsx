2abd03cf6bf077b417776686035c08b83c8ddc10
Alexander <valverin95@gmail.com>
2022-10-19 02:42:42 +0300

    feat: updated main template

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import {AppRouters} from 'routers';
 import type {Component} from 'solid-js';
+import {Footer} from 'template';
 
 const App: Component = () => {
   return (
-    <main>
-      <header>Header</header>
-      <AppRouters />
+    <main class="relative h-screen flex flex-col">
+      <div class="navbar bg-base-200"></div>
+      <div class="flex-1 h-full overflow-y-scroll">
+        <AppRouters />
+      </div>
+      <Footer />
     </main>
   );
 };
